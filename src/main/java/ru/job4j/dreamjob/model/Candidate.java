871d49e124347438055a4168cbf4cca49d3f86e6
yor_email <daniil.stebnitsky@gmail.com>
2022-05-19 13:05:08 +0300

    5. Список Вакансий [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -0,0 +1,60 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Candidate {
+
+    private int id;
+    private String name;
+    private String description;
+    private String created;
+
+    public Candidate() {
+    }
+
+    public Candidate(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        created = LocalDate.now().toString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Candidate)) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
