977231f19ceb3a930500e6b1d47c03c279b47739
yor_email <daniil.stebnitsky@gmail.com>
2022-05-26 19:42:47 +0300

    2. Связь слоев через Spring DI. [#504856]

--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -1,25 +1,24 @@
 package ru.job4j.dreamjob.store;
 
+import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+@Repository
 public class CandidateStore {
 
-    private static final CandidateStore INST = new CandidateStore();
-
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private final AtomicInteger ids;
 
-    private CandidateStore() {
+    public CandidateStore() {
         candidates.put(1, new Candidate(1, "Junior Java dev.", "Experience: 1 year."));
         candidates.put(2, new Candidate(2, "Middle Java dev.", "Experience: 3 years."));
         candidates.put(3, new Candidate(3, "Senior Java dev.", "Experience: 5 years."));
-    }
-
-    public static CandidateStore instOf() {
-        return INST;
+        ids = new AtomicInteger(candidates.size());
     }
 
     public Collection<Candidate> findAll() {
@@ -27,6 +26,7 @@ public class CandidateStore {
     }
 
     public void add(Candidate candidate) {
+        candidate.setId(ids.incrementAndGet());
         candidates.put(candidate.getId(), candidate);
     }
 
--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -1,25 +1,25 @@
 package ru.job4j.dreamjob.store;
 
+import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Post;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+
+@Repository
 public class PostStore {
 
-    private static final PostStore INST = new PostStore();
-
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicInteger ids;
 
-    private PostStore() {
+    public PostStore() {
         posts.put(1, new Post(1, "Junior Java Job", "J. description"));
         posts.put(2, new Post(2, "Middle Java Job", "M. description"));
         posts.put(3, new Post(3, "Senior Java Job", "S. description"));
-    }
-
-    public static PostStore instOf() {
-        return INST;
+        ids = new AtomicInteger(posts.size());
     }
 
     public Collection<Post> findAll() {
@@ -27,6 +27,7 @@ public class PostStore {
     }
 
     public void add(Post post) {
+        post.setId(ids.incrementAndGet());
         posts.put(post.getId(), post);
     }
 
